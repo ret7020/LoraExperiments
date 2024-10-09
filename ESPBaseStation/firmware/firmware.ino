a38eab967242ebb4d9995ff5536cc3ec48e51125
ret7020 <ret7020@gmail.com>
2024-10-10 01:32:45 +0300

    Some config stuff; untested

--- ESPBaseStation/firmware/firmware.ino
+++ ESPBaseStation/firmware/firmware.ino
@@ -30,10 +30,20 @@ AsyncWebServer server(HTTP_PORT);
 AsyncWebSocket ws("/ws");
 JSONVar packetFromSocket;
 
+double bwToInt[10] = {
+    7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, 500E3
+};
+uint32_t delayBetweenMessages = 1000; // ms
+uint8_t packetCycleCounter = 0;
+uint8_t txEnabled = 1;
+
+
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     packetFromSocket = JSON.parse((char*)data);
-    // Packet struct
+
     /*
+    Packet struct
+
     // action == 0 - configure LoRa options
     {"action": 0, "bw": 12, "sf": int, "cr", "pl": int, "crc": bool, "txp": int, "iq": bool, "dl": int}
         bw - Bandwidth
@@ -44,7 +54,36 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
         txp - tx power
         iq - Invert I/Q Signals
         dl - delay between sending packages in ms
+
+    // action == 1 - reset packet cycle var
+    {"action": 1}
+    // action == 2 - invert TX enable status (toggle on/off)
     */
+    switch (packetFromSocket["action"]){
+        case 0:
+            LoRa.setSignalBandwidth(bwToInt[packetFromSocket["bw"]]);
+            LoRa.setSpreadingFactor(packetFromSocket["sf"]);
+            LoRa.setCodingRate4(packetFromSocket["cr"]);
+            LoRa.setPreambleLength(packetFromSocket["pl"]);
+            if (packetFromSocket["pl"]) LoRa.enableCrc();
+            else LoRa.disableCrc();
+            LoRa.setTxPower(packetFromSocket["txp"]);
+            delayBetweenMessages = packetFromSocket["dl"];
+            if (packetFromSocket["iq"]) LoRa.enableInvertIQ();
+            else LoRa.disableInvertIQ();
+            
+            break;
+        
+        case 1:
+            packetCycleCounter = 0;
+            break;
+
+        case 2:
+            txEnabled = !txEnabled;
+            break;
+    }
+    
+
 }
 
 void websocketEventHandler(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
@@ -106,6 +145,6 @@ void loop()
     //
     //  counter++;
     //
-    delay(1000);
+    // if (delayBetweenMessages)
     ws.cleanupClients();
 }

4924698968ed13912768e7c524347bd9c75ed6e8
ret7020 <ret7020@gmail.com>
2024-11-20 01:43:05 +0300

    Heltec modules tests

---
+++ ESPBaseStation/firmware/heltec_sx1262_TX.c
@@ -0,0 +1,45 @@
+#include <RadioLib.h>
+#include <SPI.h>
+
+#define LORA_NSS 8
+#define LORA_SCK 9
+#define LORA_MOSI 10
+#define LORA_MISO 11
+#define LORA_RST 12
+#define LORA_BUSY 13
+#define LORA_DIO1 14
+
+
+byte packet[5] = {255, 200, 145, 90, 35};
+
+// NSS DIO1 NRST BUSY
+SX1262 radio = new Module(LORA_NSS, LORA_DIO1, LORA_RST, LORA_BUSY);
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_NSS);
+  Serial.print(F("[SX1262] Initializing ... "));
+  int state = radio.begin(868.0, 500.0, 12, 5, 0x12, 20, 10);
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
+
+//  state = radio.setOutputPower(20);
+//  Serial.println(state);
+  
+  
+
+}
+
+void loop(){
+  int state = radio.transmit(packet, 5);
+  if (state == RADIOLIB_ERR_NONE){
+    Serial.println("[SX1262] TX OK");
+  }
+  delay(1000);
+
+}
